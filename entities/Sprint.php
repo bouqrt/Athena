015b6f52508fb49a2cde054853278a9fb6e13d9e
bouqrt <bouchraelqrt@gmail.com>
2025-12-31 11:36:04 +0100

    addition of setters and getters for the sprint class

--- entities/Sprint.php
+++ entities/Sprint.php
@@ -14,6 +14,33 @@ class Sprint{
         $this->end_date =$end_date;
     }
 
-    
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getProjectId(){
+        return $this->project_id;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+    public function setName($name){
+        $this->name =$name;
+    }
+
+    public function getStartDate(){
+        return $this->start_date;
+    }
+    public function setStartDate($start_date){
+        $this->start_date =$start_date;
+    }
+
+    public function getEndDate(){
+        return $this->end_date;
+    }
+    public function setEndDate($end_date){
+        $this->end_date =$end_date;
+    }
 
 }
