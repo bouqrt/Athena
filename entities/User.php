c42a27c1efb118d535a25f7809f269ce9c5eb56b
bouqrt <bouchraelqrt@gmail.com>
2025-12-31 11:07:41 +0100

    first commit :file structure and creation of the user class

---
+++ entities/User.php
@@ -0,0 +1,63 @@
+<?php
+
+class User{
+    public $id;
+    public $name;
+    public $email;
+    public $password;
+    public $role;
+    public $status;
+    public $created_at;
+
+
+    public function __construct($name,$email,$password,$role= 'member'){
+        $this->name = $name; 
+        $this->email = $email;
+        $this->password = $password;
+        $this->role = $role;
+        $this->status = $true;
+    }
+
+    public function getId(){
+        return $this->id ;
+    }
+
+    public function getName(){
+        return $this->name ;
+    }
+    public function setName($name){
+        $this->name = $name;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+    public function setEmail($email){
+        $this->email =$email;
+    }
+
+    public function getPassword(){
+        return $this->password;
+    }
+    public function setPassword($password){
+        $this->password =$password;
+    }
+
+    public function getRole(){
+        return $this->role;
+    }
+    public function setRole($role){
+        $this->role =$role;
+    }
+
+    public function isActive(){
+        return $this->status;
+    }
+    public function setStatus($status){
+        $this->status =$status;
+    }
+
+    public function getCreatedAt(){
+        return $this->created_at;
+    }
+}
