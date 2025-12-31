99741e4a51babe11539f96d82ac3df669587ff58
bouqrt <bouchraelqrt@gmail.com>
2025-12-31 11:12:23 +0100

    added the getters and setters into the project class

--- entities/Project.php
+++ entities/Project.php
@@ -16,4 +16,49 @@ class Projet{
         $this->status ='planned';
     }
 
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getTitle(){
+        return $this->title;
+    }
+    public function setTitle($title){
+        $this->title =$title;
+    }
+
+    public function getDescription(){
+        return $this->description;
+    }
+    public function setDescription($description){
+        $this->description =$description;
+    }
+
+    public function getStartDate(){
+        return $this->start_date;
+    }
+    public function setStartDate($start_date){
+        $this->start_date =$start_date;
+    }
+
+    public function getEndDate(){
+        return $this->end_date;
+    }
+    public function setEndDate($end_date){
+        $this->end_date =$end_date;
+    }
+
+    public function getStatus(){
+        return $this->status;
+    }
+    public function setStatus($status){
+        $this->status =$status;
+    }
+
+    public function getManagerId(){
+        return $this->manager_id;
+    }
+
+}
+
     
